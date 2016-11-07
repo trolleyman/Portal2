060419507036ff4bfcdc2a71a789ed26652a7de3
Callum Tolley <cgtrolley@gmail.com>
2016-11-07 11:12:24 +0000

    Fixed vfs relative_to_exe

--- src/vfs.rs
+++ src/vfs.rs
@@ -5,6 +5,7 @@ use std::env::current_exe;
 /// Makes a path relative to the executable (or if it can't find it, the current directory)
 pub fn relative_to_exe(p: &str) -> PathBuf {
 	let mut filepath = current_exe().unwrap_or(PathBuf::from("."));
+	filepath.pop();
 	filepath.push(p);
 	filepath
 }
