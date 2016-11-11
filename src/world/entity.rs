8da7a3534246425a8106fde7f6dedb716c9d2d8e
Callum Tolley <cgtrolley@gmail.com>
2016-11-11 21:11:37 +0000

    Added transforms

--- src/world/entity.rs
+++ src/world/entity.rs
@@ -1,22 +1,88 @@
 use prelude::*;
 
+use glium::Frame;
+
+use render::Render;
 use render::mesh::MeshID;
+
+#[derive(Debug, Clone)]
+pub struct Transform {
+	pos: Vec3,
+	rot: Quat,
+	scale: Vec3,
+	mat: Mat4,
+}
+impl Default for Transform {
+	fn default() -> Transform {
+		Transform::from_pos(zero())
+	}
+}
+impl Transform {
+	pub fn new(pos: Vec3, rot: Quat, scale: Vec3) -> Transform {
+		let mut t = Transform {
+			pos: pos,
+			rot: rot,
+			scale: scale,
+			mat: one(),
+		};
+		t.recalc_mat();
+		t
+	}
+	pub fn from_pos(pos: Vec3) -> Transform {
+		Transform::new(pos, one(), vec3(1.,1.,1.))
+	}
+	
+	fn recalc_mat(&mut self) {
+		self.mat = Mat4::from_translation(self.pos)
+		         * Mat4::from(self.rot)
+		         * Mat4::from_nonuniform_scale(self.scale.x, self.scale.y, self.scale.z);
+	}
+	
+	pub fn pos(&self) -> Vec3 {
+		self.pos
+	}
+	pub fn rot(&self) -> Quat {
+		self.rot
+	}
+	pub fn scale(&self) -> Vec3 {
+		self.scale
+	}
+	pub fn mat(&self) -> Mat4 {
+		self.mat
+	}
+	pub fn set_pos(&mut self, pos: Vec3) {
+		self.pos = pos;
+		self.recalc_mat();
+	}
+	pub fn set_rot(&mut self, rot: Quat) {
+		self.rot = rot;
+		self.recalc_mat();
+	}
+	pub fn set_scale(&mut self, scale: Vec3) {
+		self.scale = scale;
+		self.recalc_mat();
+	}
+}
+impl From<Vec3> for Transform {
+	fn from(pos: Vec3) -> Transform {
+		Transform::from_pos(pos)
+	}
+}
 
 #[allow(dead_code)]
 pub struct Entity {
-	pos: Vec3,
-	mesh: MeshID,
+	pub trans: Transform,
+	pub mesh: MeshID,
 }
 impl Entity {
-	pub fn new(pos: Vec3, mesh: MeshID) -> Entity {
-		Entity::new_ext(pos, zero(), one(), mesh)
-	}
-	
-	pub fn new_ext(pos: Vec3, _scale: Vec3, _rot: Quat, mesh: MeshID) -> Entity {
+	pub fn new<T: Into<Transform>>(trans: T, mesh: MeshID) -> Entity {
 		Entity {
-			pos: pos,
-			// TODO: Calculate rotation & scaling
+			trans: trans.into(),
 			mesh: mesh,
 		}
+	}
+	
+	pub fn render(&self, _r: &mut Render, _f: &mut Frame) {
+		unimplemented!()
 	}
 }
